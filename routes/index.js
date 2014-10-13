40185fb380cceef78ae706083b65a9d088c9d367
Greg <gredelston@gmail.com>
2014-10-13 12:37:19 -0400

    trying to start some db stuffffff?

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/', function (req, res) {
+  var user = olinapps.user(req);
+  console.log(user);
+  res.render('index', {title: user.username})
+})
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
 
+router.post('/test',function(req, res) {
+	res.send("This is a test. This is only a test.");
+});
+
 module.exports = router;
